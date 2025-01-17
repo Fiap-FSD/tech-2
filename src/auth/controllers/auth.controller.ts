729aa9c335f6fb3fa7d1b353c962ef2c3a20c493
JoaoA1egre <guivogelsanguer@gmail.com>
2025-01-16 21:46:32 -0300

    Detalhamento na Swagger

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -2,6 +2,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from '../services/auth.service'; 
 import { UserService } from 'src/user/services/user.service'; 
+import { ApiBody, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -11,12 +12,49 @@ export class AuthController {
     ) {}
 
     @Post('login')
+    @ApiBody({
+        description: 'login de exemplo.',
+        schema: {
+            type: 'object',
+            properties: {
+                email: { type: 'string', example: 'marcelo@teste.com' },
+                password: { type: 'string', example: 'senha123' },
+            },
+        },
+    })
+    @ApiResponse({
+        status: 201,
+        description: 'retorno do access_token',
+        example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hcmNlbG9AdGVzdGUuY29tIiwic3ViIjoiNjc4ODc2N2FmNzk4MmRlNTE1MGQ5MDk3Iiwicm9sZSI6InVzZXIiLCJpYXQiOjE3MzcwNzM2NjcsImV4cCI6MTczNzA3NzI2N30.TYgESpm65v0_JI8eMHtYRMm5FFqmBQp3DRyasz3crgQ'
+    })
+    @ApiResponse({
+        status: 401,
+        description: 'Invalid credentials.',
+    })
     async login(@Body() body: { email: string; password: string }) {
         const user = await this.authService.validateUser(body.email, body.password);
         return this.authService.login(user);
     }
 
     @Post('register')
+    @ApiBody({
+        description: 'login a ser criado.',
+        schema: {
+            type: 'object',
+            properties: {
+                email: { type: 'string', example: 'marcelo@teste.com' },
+                password: { type: 'string', example: 'senha123' },
+            },
+        },
+    })
+    @ApiResponse({
+        status: 201,
+        description: 'usuario criado com sucesso.',
+    })
+    @ApiResponse({
+        status: 409,
+        description: 'email em uso.',
+    })
     async register(@Body() body: { email: string; password: string }) {
         return this.userService.createUser(body);
     }
